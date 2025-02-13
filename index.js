ee506443fb239b1252c1bba877abe13a79189701
arafatDU <bsse1435@iit.du.ac.bd>
2025-02-13 14:39:39 +0600

    Used stream both read and write for zip a file

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const status = require("express-status-monitor");
 const fs = require("fs");
+const zlib = require("zlib");
 
 const app = express();
 const PORT = 8000;
 
 app.use(status());
+
+
+// fs.createReadStream("./sample.txt").pipe(
+// 	zlib.createGzip().pipe(fs.createWriteStream("./sample.zip"))
+// );
 
 app.get("/", (req, res) => {
 	const stream = fs.createReadStream("./OxfordDictionary.txt", "utf-8");
